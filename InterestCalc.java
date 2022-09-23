a341116a3b0ae155d41efb1917fe49ce88c52bb9
Arc <98681867+Arc-Ninja@users.noreply.github.com>
2022-09-23 18:19:08 +0530

    Add files via upload

---
+++ InterestCalc.java
@@ -0,0 +1,18 @@
+//import java.io.*;
+//
+//public class InterestCalc {
+//    public static void main(String[] args) {
+//        DataInputStream in = new DataInputStream(System.in);
+//        System.out.println("Enter PA: ");
+//        System.out.flush();
+//        Float PA = in.readFloat();
+//        System.out.println("Enter Rate: ");
+//        System.out.flush();
+//        Float Rate = in.readFloat();
+//        System.out.println("Enter Year: ");
+//        System.out.flush();
+//        int Year = in.readInt();
+//        float TotalInterest = PA*Rate*Year;
+//        System.out.println("TotalInterest: " + TotalInterest);
+//    }
+//}
---
+++ appletdemo.java
@@ -0,0 +1,40 @@
+import java.applet.*;
+import java.awt.*;
+public class testing extends Applet{
+	Font f;
+    public void init(){
+		f = new Font("Arial", Font.BOLD, 26);
+		resize(500,300);
+		setBackground(Color.BLACK);
+		setForeground(Color.GREEN);
+	
+}
+    public void paint(Graphics g){
+        int x = 0;
+		int y = 0;
+		String msg = "";
+		x = Integer.parseInt(getParameter("xPosition"));
+		y = Integer.parseInt(getParameter("yPosition"));
+		msg = getParameter("msg");
+		g.setFont(f);
+		g.drawString(msg, x, y);
+		
+		
+    }
+}
+
+/*<html>
+<title> My Applet</title>
+<hr>
+
+<applet code="testing.class" width="300" height="300">
+
+<param name="msg" value="This is a test program"/>
+
+<param name="xPosition" value= "150"/>
+
+<param name="yPosition" value="50"/>
+
+</applet>
+</hr>
+</html>*/
